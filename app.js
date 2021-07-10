a189fe615f51f3b76a3a67b67002738d6ffbdc7b
sukesh2000 <sukesh.seth2000@gmail.com>
2021-07-11 03:06:25 +0530

    Added routes for register

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const port = process.env.PORT || 5000;
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+const authRoute = require('./routes/auth');
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+app.use('/api/user', authRoute);
+
+mongoose.connect(process.env.DB_CONNECT, {useUnifiedTopology: true, useNewUrlParser: true}).then(()=>{
+
+    console.log('Database is connected');
+    
+    })
+    .catch(err => {
+        console.log(err.message);
+}); 
+
+app.listen(port, ()=>{
+    console.log('Server is running on port %s', port);
+})
