eb79c7674764dec41534ce79b776942d743df2d4
sukesh2000 <sukesh.seth2000@gmail.com>
2021-07-11 16:27:37 +0530

    Added credetials validation & route for login

--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
+const bcrypt = require("bcryptjs");
+const { registerValidation, loginValidation } = require("../models/validation")
 
 router.post('/register', async(req, res)=>{
+
+    const { error } = registerValidation(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+
     const emailExist = await User.findOne({email: req.body.email});
     if(emailExist) return res.status(400).send('Email already exists');
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
     const newUser = new User({
         username: req.body.username,
         email: req.body.email,
-        password: req.body.password
+        password: hashedPassword
     });
     try{
         const savedUser = await newUser.save();
@@ -19,5 +28,19 @@ router.post('/register', async(req, res)=>{
         res.status(400).send(err);
     }
 })
+
+router.post('/login',async(req,res)=>{
+    
+    const { error } = loginValidation(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+
+    const user = await User.findOne({email:req.body.email});
+    if(!user) return res.status(400).send('Email is not registered');
+
+    const validPass = await bcrypt.compare(req.body.password, user.password);
+    if(!validPass) return res.status(400).send('Invalid Passowrd');
+
+    res.send('Logged in!');
+});
 
 module.exports = router;
