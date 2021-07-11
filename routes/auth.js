9c48c4c233200984869dda6aca824488bfef269d
sukesh2000 <sukesh.seth2000@gmail.com>
2021-07-11 16:31:52 +0530

    Added credentials validation & route for login

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 const bcrypt = require("bcryptjs");
-const { registerValidation, loginValidation } = require("../models/validation")
+const { registerValidation, loginValidation } = require("../validation")
 
 router.post('/register', async(req, res)=>{
 
