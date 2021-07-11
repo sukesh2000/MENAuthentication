990b2bc5b5b9d2c095c4de8cb58d42be7870c38f
sukesh2000 <sukesh.seth2000@gmail.com>
2021-07-11 16:43:13 +0530

    Added password encryption

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
-const bcrypt = require("bcryptjs");
+const bcrypt = require("bcryptjs"); 
 const { registerValidation, loginValidation } = require("../validation")
 
 router.post('/register', async(req, res)=>{
